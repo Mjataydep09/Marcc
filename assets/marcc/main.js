b7df5bb72b3aeef2e2e092b0ee1e5228dd795281
mjatayde09 <marccjoseph06@gmail.com>
2020-06-24 14:35:12 +0400

    a

---
+++ assets/marcc/main.js
@@ -0,0 +1,22 @@
+
+function onmouse1(x){
+  x.style.fill='#ff7272';
+}
+function onmouse2(x){
+  x.style.fill='#831848';
+}
+function onmouse3(x){
+  x.style.fill='#88e74f';
+}
+function onmouse4(x){
+  x.style.fill='#4fe7c9';
+}
+function onmouse5(x){
+  x.style.fill='#764fe7';
+}
+function onmouse6(x){
+  x.style.fill='#48454f';
+}
+function onmouse7(x){
+  x.style.fill='#ef50de';
+}
